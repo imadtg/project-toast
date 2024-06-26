084e22a34e431a18b04ed712de2306c7c89174a8
imadtg <i.haidi@outlook.com>
2024-06-26 16:19:34 +0100

    small refactor

--- src/hooks/use-on-key.js
+++ src/hooks/use-on-key.js
@@ -1,14 +1,14 @@
 import React from "react";
 
-export default function useOnKey(key, callback) {
+export default function useOnKey(keycode, callback) {
   React.useEffect(() => {
     function handleKeyDown(event, ...args) {
-      if (event.code === key) {
+      if (event.code === keycode) {
         callback(event, ...args);
       }
     }
     window.addEventListener("keydown", handleKeyDown);
 
     return () => window.removeEventListener("keydown", handleKeyDown);
-  }, [key, callback]);
+  }, [keycode, callback]);
 }
